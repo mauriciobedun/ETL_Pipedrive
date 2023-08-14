fa26163d8d96f04654b84fd853fd45dfe9004658
Mauricio Bedun <mauriciobedun@hotmail.com>
2023-08-14 12:08:52 -0300

    teste

--- paginacao_api1.py
+++ paginacao_api1.py
@@ -74,3 +74,71 @@ while True:
 
 # Fechar a conexão
 conn.close()
+
+
+## teste com funções
+
+# import requests
+# import os
+# from dotenv import load_dotenv
+# import pyodbc
+
+# # Load environment variables
+# load_dotenv()
+
+# base_url = os.environ.get("base_url")
+# api_token = os.environ.get("api_token")
+# Server = os.environ.get("Server")
+# database = os.environ.get("database")
+# UID = os.environ.get('UID')
+# PWD = os.environ.get('PWD')
+
+# def main():
+#     # Connect to the database
+#     with pyodbc.connect(
+#         Driver='{SQL Server Native Client 11.0}',
+#         Server=Server,
+#         database=database,
+#         UID=UID,
+#         PWD=PWD
+#     ) as conn:
+#         cursor = conn.cursor()
+
+#         endpoint = "deals"
+#         page = 1
+#         per_page = 500
+#         total_items = None
+
+#         while total_items is None or (page - 1) * per_page < total_items:
+#             params = {
+#                 "api_token": api_token,
+#                 "start": (page - 1) * per_page,
+#                 "limit": per_page
+#             }
+
+#             response = requests.get(f"{base_url}{endpoint}", params=params)
+#             status_code = response.status_code
+
+#             if status_code == 200:
+#                 data = response.json()
+#                 total_items = data["total"]
+#                 items = data["data"]
+
+#                 if not items:
+#                     break
+
+#                 batch_values = [(item["id"], item["title"], item["value"]) for item in items]
+#                 insert_query = "INSERT INTO [SDG_CRM].[dbo].[deals] (id, title, value) VALUES (?, ?, ?)"
+#                 cursor.executemany(insert_query, batch_values)
+#                 conn.commit()
+
+#                 for deal_id, deal_title, deal_value in batch_values:
+#                     print(f"Deal inserido: ID = {deal_id}, Título = {deal_title}, Valor = {deal_value}")
+                
+#                 page += 1
+#             else:
+#                 print("Erro na solicitação:", status_code)
+#                 break
+
+# if _name_ == "_main_":
+#     main()
